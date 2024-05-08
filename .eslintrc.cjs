bcfa3baaa556e6810faa9d61e8cbaed75c928d8c
croot <croot.dev@gmail.com>
2024-05-08 22:58:49 +0900

    .

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,41 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
 module.exports = {
-  env: {
-    browser: true,
-    node: true,
-  },
+  root: true,
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  plugins: ['stylelint', 'import'],
   parser: 'vue-eslint-parser',
   parserOptions: {
     parser: '@typescript-eslint/parser',
-    sourceType: 'module',
+    ecmaVersion: 'latest'
   },
-  plugins: ['@typescript-eslint', 'vue'],
-  extends: [
-    'eslint:recommended',
-    'stylelint',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:vue/vue3-recommended',
-    'plugin:prettier/recommended',
-  ],
-};
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'type',
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'unknown'
+        ],
+        pathGroupsExcludedImportTypes: ['@tanstack*'],
+        alphabetize: {
+          order: 'asc'
+        }
+      }
+    ]
+  }
+}
